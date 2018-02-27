7c239d937859d23793663ffa9cd9b41c9eaa3898
Alejandro Anzures <alejandreyes36@gmail.com>
2018-02-26 21:29:19 -0600

    Initial commit

---
+++ Sistemas Operativos/Tareas/Tarea02/Main/main.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char puntero = "saas";
+int main() {
+    
+    
+    printf("%c",puntero) ; 
+    printf("Alejandreyes");
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
+
